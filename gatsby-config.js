07d8a98eeba2642ff63cadbe9eaecb2a3fc5a633
Tam Le <philosocode@gmail.com>
2020-12-28 09:50:43 -0700

    Remove analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,12 +49,6 @@ module.exports = {
         icon: `src/images/blaze-logo.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-173067610-1",
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
